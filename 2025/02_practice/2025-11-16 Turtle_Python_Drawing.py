440f4e283df02c561efe5d1ec2ccca4d6a720890
Julian Wong <JulianWong310@gmail.com>
2025-11-24 15:55:55 -0500

    Add 2025 folder with Python practice files

---
+++ 2025/02_practice/2025-11-16 Turtle_Python_Drawing.py
@@ -0,0 +1,17 @@
+"""
+2025/11/16
+use python Turtle to draw a sail
+practice for-loop
+"""
+import turtle as x
+x.bgcolor("black") # set background colour
+x.shape("turtle") # set pen shape
+x.pencolor("blue") # set pen colour
+x.speed(100) # set drawing speed
+r = 10 # r is the radius of the circle
+for i in range(45):
+    x.left(10) # the direction and angel of each tilt
+    x.circle(r)
+    r=r+3
+x.done()
+
