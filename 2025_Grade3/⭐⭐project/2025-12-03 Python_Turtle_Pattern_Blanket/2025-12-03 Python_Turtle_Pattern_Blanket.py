9a2ecde1e203a7ed1e49b3f843d2c8c2401a6d65
JulianWong310 <julianwong310@gmail.com>
2025-12-03 22:18:18 -0500

    更新 2025-12-03 Python_Turtle_Pattern_Blanket.py

--- 2025_Grade3/⭐⭐project/2025-12-03 Python_Turtle_Pattern_Blanket/2025-12-03 Python_Turtle_Pattern_Blanket.py
+++ 2025_Grade3/⭐⭐project/2025-12-03 Python_Turtle_Pattern_Blanket/2025-12-03 Python_Turtle_Pattern_Blanket.py
@@ -4,8 +4,8 @@
 This project uses Python Turtle
 to draw a colorful blanket filled with fun shapes.
 The picture includes:
-•  A pink blanket background
-•	 A cyan square
+• A pink blanket background
+•	A cyan square
 •	A  blue circle
 •	A red flower made with repeated arcs
 •	A yellow star
