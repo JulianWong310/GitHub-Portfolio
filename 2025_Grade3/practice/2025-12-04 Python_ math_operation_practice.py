bddcaeeca5e8f13b9d09b3c71c96f8da2918d1a3
JulianWong310 <julianwong310@gmail.com>
2025-12-04 19:38:27 -0500

    Create 2025-12-04 Python_ math_operation_practice.py

---
+++ 2025_Grade3/practice/2025-12-04 Python_ math_operation_practice.py
@@ -0,0 +1,81 @@
+"""
+2025/12/04
+Python Math Operator Practice
+🎯 Simple Rule
+👉 Use + − * / for normal math
+👉 Use // to drop decimals
+👉 Use % to get remainders
+👉 Use ** to do powers
+"""
+# Practice 1: Addition
+# Create two variables and add them together.
+x=60
+v=7
+z=x+v
+print(z)
+
+# Practice 2: Subtraction
+# Make two numbers and subtract.
+t=70
+k=3
+r=t-k
+print(r)
+
+#Practice 3: Multiplication
+# Multiply two numbers.
+r=1
+s=67
+print(r*s)
+
+# Practice 4: Division
+# Divide two numbers and see the decimal result.
+j=1
+p=67
+print(p/j)
+
+# Practice 5: Floor Division (//)
+# Find how many whole groups you can make.
+o=4490
+i=67
+print(o//i)
+
+# Practice 6: Modulo (%)
+# Find the remainder.
+x=4691
+v=68
+print(x%v)
+
+# Practice 7: Even or Odd
+# Use % to check if a number is even or odd.
+q=67
+if q%2==0:
+    print("it is even")
+else:
+    print("it is odd")
+
+#Practice 8: Exponent ( )**
+# Write a power (like 2³).
+y=67
+x=1
+print(y**x)
+
+# Practice 9: Mixed Story Problem
+# Write code for this story:
+# You have 97 apples.
+# You share them with 6 friends.
+# How many apples does each friend get?
+# How many apples are left?
+w=97
+l=6
+x=1
+print(w//l)
+print(w%l)
+
+# Practice 10: Make Your Own
+# Create your own math problem using any operator and solve it with Python.
+# Calvin has 4489 presents.
+# Calvin gives presents to 67 kids
+# How many presents will each kid get?
+d=4489
+n=67
+print(d/n)
