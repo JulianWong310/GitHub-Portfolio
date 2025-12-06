cace304d12f776c92bad3a772c85160dd5cc781a
JulianWong310 <julianwong310@gmail.com>
2025-12-06 17:46:10 -0500

    Create 2025-12-06 Python_f-strings_practice.py

---
+++ 2025_Grade3/practice/2025-12-06 Python_f-strings_practice.py
@@ -0,0 +1,41 @@
+"""
+2025/12/06
+Practice f-strings to write sentences about
+my name, age, favorite things, math problems, and a story.
+"""
+
+# Practice 1: My Name
+# Make a variable for my name and print a sentence using an f-string.
+name = "Piranna Plant"
+print(f"My name is {name}.")
+
+# Practice 2: My Age
+# Make a variable for my age and print.
+age = 10
+print(f"My age is {age}.")
+
+# Practice 3: Favorite Game
+# Make a variable for my favorite animal and print a sentence.
+favorite_game = "Super Mario 3D World"
+print(f"My favorite game is {favorite_game}.")
+
+# Practice 4: Favorite Food Story
+# Make variables for my name and favorite food, then print.
+name1 = "Ludwig"
+favorite_food = "Mc Donalds"
+print(f"{name1}'s favorite food is {favorite_food}.")
+
+# Practice 5: Math With f-strings
+# Create two numbers and print the answer inside a sentence.
+num1 = 9
+num2 = 7
+num3 = num1 * num2
+print(f"{num1} *  {num2} = {num3} ")
+
+# Practice 6: Character Story
+# Make variables for: hero's name,superpower, and mission.
+# Then print a short story using an f-string.
+hero_name = "Jangmo-o"
+superpower = "dragon tail"
+mission = "to win the battle against Gingar"
+print(f"{hero_name}'s superpower is {superpower}, his mission is {mission}.")
