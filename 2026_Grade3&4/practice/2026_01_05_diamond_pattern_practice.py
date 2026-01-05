31dd3300a9d3edcb0479651d0a573c4879078de8
JulianWong310 <julianwong310@gmail.com>
2026-01-05 18:16:34 -0500

    更新 2026_01_05_diamond_pattern_practice.py

--- 2026_Grade3&4/practice/2026_01_05_diamond_pattern_practice.py
+++ 2026_Grade3&4/practice/2026_01_05_diamond_pattern_practice.py
@@ -10,8 +10,8 @@ Draw a diamond (rhombus) pattern
 for i in range(1,6,2):
     star="*"*i
     space=" "*((5-i)//2)
-    print(space,star,space)
+    print(space+star+space)
 for i in range(3,0,-2):
     star="*"*i
     space=" "*((5-i)//2)
-    print(space,star,space)
+    print(space+star+space)
