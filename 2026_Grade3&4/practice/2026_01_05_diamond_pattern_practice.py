98fad85595965bdbca4884741d4b48ad07657c93
JulianWong310 <julianwong310@gmail.com>
2026-01-05 17:40:53 -0500

    更新 2026_01_05_diamond_pattern_practice.py

--- 2026_Grade3&4/practice/2026_01_05_diamond_pattern_practice.py
+++ 2026_Grade3&4/practice/2026_01_05_diamond_pattern_practice.py
@@ -1,10 +1,10 @@
 """
 2026-01-05
-Draw a Diamond (Rhombus) Pattern
+Draw a diamond (rhombus) pattern
       *
-    ***
-  *****
-    ***
+     ***
+    *****
+     ***
       *
 """
 for i in range(1,6,2):
