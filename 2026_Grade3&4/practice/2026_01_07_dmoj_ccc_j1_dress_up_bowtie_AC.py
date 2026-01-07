45df42b07d94c027e26967eb24fcf2a25588cff3
JulianWong310 <julianwong310@gmail.com>
2026-01-07 17:42:36 -0500

    更新 2026_01_07_dmoj_ccc_j1_dress_up_bowtie_AC.py

--- 2026_Grade3&4/practice/2026_01_07_dmoj_ccc_j1_dress_up_bowtie_AC.py
+++ 2026_Grade3&4/practice/2026_01_07_dmoj_ccc_j1_dress_up_bowtie_AC.py
@@ -2,25 +2,29 @@
 2026-01-07
 https://dmoj.ca/problem/ccc01j1
 Your program should take as input the height H of the bow tie, where
- H is an odd positive integer greater than or equal to 5. A bow tie with
- H rows (2H  columns) should then be printed using the pattern shown below in the sample output.
+H is an odd positive integer greater than or equal to 5. A bow tie with
+H rows(2H columns) should then be printed using the pattern shown below in the sample output.
 
 Input Specification
 One line containing integer H
 You may assume that all input data will be valid.
 
 Sample Input 1
-
 5
+
 Sample Output 1
+
 *        *
 ***    ***
 **********
 ***    ***
 *        *
+
 Sample Input 2
 7
+
 Sample Output 2
+
 *            *
 ***        ***
 *****    *****
@@ -29,6 +33,7 @@ Sample Output 2
 ***        ***
 *            *
 """
+
 H=int(input())
 for i in range(1,H+1,2):
     star="*"*i
