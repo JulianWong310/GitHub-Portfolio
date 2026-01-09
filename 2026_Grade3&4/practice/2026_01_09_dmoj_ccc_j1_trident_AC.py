52bfca64961ab6e5a23e44bd1795f3ed76e65fc4
JulianWong310 <julianwong310@gmail.com>
2026-01-09 18:13:57 -0500

    change

---
+++ 2026_Grade3&4/practice/2026_01_09_dmoj_ccc_j1_trident_AC.py
@@ -0,0 +1,55 @@
+"""
+2026-01-07
+https://dmoj.ca/problem/ccc03j1
+A trident is a fork with three tines (prongs).
+A simple picture of a trident can be made from asterisks and spaces:
+
+*    *    *
+*    *    *
+*    *    *
+*******
+      *
+      *
+      *
+      *
+In this example, each tine is a vertical column of 3 asterisks.
+Each tine is separated by 2 spaces.
+The handle is a vertical column of 4 asterisks below the middle tine.
+Tridents of various shapes can be drawn by varying three parameters:
+t, the height of the tines,
+s, the spacing between tines, and
+h, the length of the handle. For the example above we have
+t=3, s=2, and h=4
+
+You are to write an interactive program to print a trident.
+Your program should accept as input the parameters
+t,s,and h and print the appropriate trident. You can assume that
+t,s,h are each at least 0 and not larger than 10.
+
+Sample Input
+4
+3
+2
+Sample Output
+
+*      *      *
+*      *      *
+*      *      *
+*      *      *
+*********
+         *
+         *.
+"""
+t=int(input())
+s=int(input())
+h=int(input())
+for i in range(t):
+    star="*"
+    space=" "*s
+    print(f"{ star}{space}{ star}{space}{ star}")
+star1="*"*(2*s+3)
+print(star1)
+for i in range(h):
+    star="*"
+    space1=" "*(s+1)
+    print(f"{space1}{star}")
---
+++ 2026_Grade3&4/practice/2026_01_09_hourglass_pattern_practice.py
@@ -0,0 +1,25 @@
+"""
+2026/01/09
+Title: Draw an Hourglass Pattern
+Problem Description:
+Write a program to print an hourglass shape using asterisks (*).
+Given a maximum width H (where H is an odd integer), the pattern
+starts with H stars, decreases to 1 star, and then increases back to H stars.
+Each line must be centered using leading spaces.
+
+Example for H=5:
+*****
+  ***
+    *
+  ***
+*****
+"""
+H=int(input())
+for i in range(H,1,-2):
+    star="*"*i
+    space=" "*((H-i)//2)
+    print(f"{space}{star}{space}")
+for i in range(1,H+2,2):
+    star="*"*i
+    space=" "*((H-i)//2)
+    print(f"{space}{star}{space}")
