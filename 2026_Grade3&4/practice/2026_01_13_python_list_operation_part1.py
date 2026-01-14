bbaa663812ee40c980d929afca4f764ebaf688f3
JulianWong310 <julianwong310@gmail.com>
2026-01-14 17:23:24 -0500

    更新 2026_01_13_python_list_operation_part1.py

--- 2026_Grade3&4/practice/2026_01_13_python_list_operation_part1.py
+++ 2026_Grade3&4/practice/2026_01_13_python_list_operation_part1.py
@@ -2,24 +2,24 @@
 2026/01/13
 Part 1
 Python List — All Common Operations
-Operation	         Modify List? 	      Use
-append()	              ✅	                 build
-pop()	                      ✅	                 simulate
-remove()	              ✅	                 delete
-count()	                  ❌	                 frequency
-sort()	                      ✅	                 ranking
-reverse()	              ✅	                 order
-len()	                      ❌	                 loops
-in	                              ❌	                 check
+Operation  Modify List? 	Use
+append()	      ✅	      build
+pop()	          ✅	      simulate
+remove()	      ✅	      delete
+count()	          ❌	      frequency
+sort()	          ✅	      ranking
+reverse()	      ✅	      order
+len()	          ❌	      loops
+in	              ❌	      check
 """
 
 # 1 Create a List
 a = [1, 2, 3, 4, 1, 5]
 b = []
-c = list("ABC")     # ['A', 'B', 'C']
+c = list("ABC") # ['A', 'B', 'C']
 print(a,b,c)
 
-# ️2 Length of a List
+# 2 Length of a List
 num=len(a)
 print(num)
 
@@ -40,7 +40,7 @@ a[2] = 0
 print(a) # [100, 6, 0]
 
 # ———— Practice ————
-# 1. Create a List
+# 1 Create a List
 # For each variable below, write YES if it is a list, NO if it is not.
 a = [1, 2, 3] # YES
 b = "123" # NO
@@ -61,7 +61,7 @@ lst=list("HELLO")
 # What is a[2] 30
 # What is a[len(a) - 1]  40
 
-# 2. Modifying by Index
+# 2 Modifying by Index
 # Given:
 a = [5, 6, 7, 8]
 # Change the first element to 100
@@ -69,8 +69,8 @@ a[0]=100
 # Change the last element to 0
 a[3]=0
 
-#3.  Index Error Check
-#Which of the following lines cause an index error?
+# 3 Index Error Check
+# Which of the following lines cause an index error?
 a = [1, 2, 3]
 a[3] = 10 # Wrong
 print(a[-1]) # Correct
