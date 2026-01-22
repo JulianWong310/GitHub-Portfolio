2d19e1adb7ad44954b966851d390ba39dbab4964
JulianWong310 <julianwong310@gmail.com>
2026-01-22 18:49:46 -0500

    Create 2026_01_22_gauss_2016_q15_pushups.py

---
+++ 2026_Grade3&4/practice/2026_01_22_gauss_2016_q15_pushups.py
@@ -0,0 +1,14 @@
+"""
+2026/01/22
+Question 15
+Sophia did push-ups every day for 7 days. Each day after the first day, she did 5 more
+push-ups than the day before. In total she did 175 push-ups. How many push-ups
+did Sophia do on the last day? justify the answer is 40.
+"""
+sum=0
+x=10
+for i in range(7):
+    sum=sum+x
+    x=x+5
+print(f"last day: {x-5}")
+print(f"total: {sum}")
