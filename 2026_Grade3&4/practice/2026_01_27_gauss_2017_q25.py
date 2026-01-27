8805e81b1859367fa455d9548456d4988aed966a
JulianWong310 <julianwong310@gmail.com>
2026-01-27 18:44:10 -0500

    Create 2026_01_27_gauss_2017_q25.py

---
+++ 2026_Grade3&4/practice/2026_01_27_gauss_2017_q25.py
@@ -0,0 +1,16 @@
+"""
+2026/01/27
+Gauss grade 7 2017 question 25
+https://cemc.uwaterloo.ca/sites/default/files/documents/2024/2017Gauss7Contest.html
+Ashley writes out the first 2017 positive integers.
+She then underlines any of the 2017 integers that is a multiple of 2,
+and then underlines any of the 2017 integers that is a multiple of 3,
+and then underlines any of the 2017 integers that is a multiple of 5.
+Finally, Ashley finds the sum of all the integers which have not been underlined.
+ What is this sum?
+"""
+sm=0
+for i in range(1,2018):
+    if i%2!=0 and  i%3!=0 and i%5!=0:
+        sm+=i
+print(sm)
