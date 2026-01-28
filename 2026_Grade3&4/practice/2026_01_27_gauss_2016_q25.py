fcd45e77c35ac239d59edb4d38400459d9c2fe6a
JulianWong310 <julianwong310@gmail.com>
2026-01-27 20:21:24 -0500

    Create 2026_01_27_gauss_2016_q25.py

---
+++ 2026_Grade3&4/practice/2026_01_27_gauss_2016_q25.py
@@ -0,0 +1,29 @@
+"""
+2026/01/27
+Gauss 2016 Question 25
+https://cemc.uwaterloo.ca/sites/default/files/documents/2024/2016Gauss7Contest.html
+"""
+# find the two closest divisors of 2016
+min_diff=2016
+for i in range(1,2017):
+    if 2016%i==0:
+        j=2016//i
+        if j>=i and  (j-i)<min_diff:
+            min_diff=j-i
+            min_i=i
+            min_j=j
+print(min_i,min_j)
+print(min_i+min_j-1)
+
+# find the closest divisors of input N
+N=int(input())
+min_diff=N
+for i in range(1,N+1):
+    if N%i==0:
+        j=N//i
+        if j>=i and  (j-i)<min_diff:
+            min_diff=j-i
+            min_i=i
+            min_j=j
+print(min_i,min_j)
+print(min_i+min_j-1)
